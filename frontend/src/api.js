8095f66ddba4c089e0f2bfd777310511085de01c
ankitkvishwakarma <ankit789.en@gmail.com>
2025-11-18 23:23:36 +0530

    modification

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // tumhara backend
+  baseURL: import.meta.env.VITE_API_URL, // tumhara backend
 });
 
 export default api;
